9e9946dba67857505a9626065a46c8d5b2112ec4
Marta <marta.tatchyn@gmail.com>
2026-03-16 20:34:25 +0000

    Add guess section with HTML, SCSS, and JavaScript; update global styles

---
+++ src/js/guess.js
@@ -0,0 +1,4 @@
+const num = undefined
+const guess = document.querySelector('.guess__number')
+const btn = document.querySelector('.guess__check')
+const answer = document.querySelector('.guess__answer')
