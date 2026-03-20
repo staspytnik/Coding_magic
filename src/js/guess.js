0cb5594393dcb60cfcfcaf16cddc495d12010e5d
Marta <marta.tatchyn@gmail.com>
2026-03-20 16:25:57 +0000

    Implement number guessing logic and user feedback in guess.js; add input validation

--- src/js/guess.js
+++ src/js/guess.js
@@ -1,4 +1,28 @@
-const num = undefined
 const guess = document.querySelector('.guess__number')
 const btn = document.querySelector('.guess__check')
 const answer = document.querySelector('.guess__answer')
+
+const check = function (e) {
+  if (guess.value !== NaN) {
+    let num = Math.round(Math.random().toFixed(1) * 10)
+    console.log(num) //!
+    if (+guess.value === num) {
+      answer.textContent = `Вітаю, ви вгадали число! (${num}) `
+      answer.style.color = '#039900'
+    }
+    if (+guess.value !== num) {
+      answer.textContent = `Ви програли, комп’ютер загадав (${num}) `
+      answer.style.color = '#900'
+    }
+  }
+
+  if (guess.value === NaN) {
+    alert('Це не число')
+  }
+}
+
+btn.addEventListener('click', check)
+
+guess.addEventListener('change', (e) => {
+  answer.textContent = ''
+})
