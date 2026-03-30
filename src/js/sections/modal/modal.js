fbdc41f456ffbdb69a8bada05858e17e678d5dae
IllyaVavsh <ilyavavsh@gmail.com>
2026-03-30 16:19:02 +0200

    Add close icon SVG, enhance modal and header functionality, and improve styles

--- src/js/sections/modal/modal.js
+++ src/js/sections/modal/modal.js
@@ -1,4 +1,4 @@
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.modal'); 
 
 const input = document.querySelector('.modal__input');
 const startBtn = document.querySelector('.modal__start-button');
@@ -8,10 +8,17 @@ const thanks = document.querySelector('.modal__thanks');
 
 const closeButtons = document.querySelectorAll('.modal__close-button');
 
+const welcomeText = document.querySelector('.header__welcome');
+
 startBtn.addEventListener('click', () => {
-  if (input.value.trim() !== '') {
+  const name = input.value.trim();
+
+  if (name !== '') {
     content.style.display = 'none';
     thanks.style.display = 'block';
+
+    welcomeText.textContent = `Вітаємо, ${name}!`;
+
   } else {
     alert('Введіть ім’я');
   }
